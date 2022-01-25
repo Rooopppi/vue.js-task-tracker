97158b22e0f98c25bc79465229211bb393f0eaab
Rooopppi <denisrooopppi@gmail.com>
2022-01-25 13:29:39 +0200

    finished

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>Version 1.0.0</h3>
+    <router-link to="/">Go Back</router-link>
+  </div>
+</template>
+
+<script>
+import Footer from '../components/Footer.vue';
+export default {
+  name: 'About',
+  // in
+    // FooterheritAttrs: false, // disable 'non-props' warning
+};
+</script>
